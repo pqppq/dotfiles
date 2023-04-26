94af9322f8d2ea655d971b1f85ec931e88e88920
pqppq <yutatsu0119@gmail.com>
2023-04-26 22:25:24 +0900

    chore: remove unused color schema

--- .config/nvim/lua/lazyvim/plugins/colors.lua
+++ .config/nvim/lua/lazyvim/plugins/colors.lua
@@ -1,21 +1,4 @@
 return {
-	'romainl/Apprentice',
-	'YorickPeterse/happy_hacking.vim',
-	'jacoborus/tender.vim',
 	'cocopon/iceberg.vim',
 	'rose-pine/neovim',
-	{
-		'sainnhe/everforest',
-		config = function()
-			vim.cmd('let g:everforest_background = "soft"')
-		end,
-	},
-	{
-		'sainnhe/edge',
-		config = function()
-			vim.cmd('let g:edge_style = "neon"')
-		end,
-	},
-	'fxn/vim-monochrome',
-	'cocopon/iceberg.vim',
 }

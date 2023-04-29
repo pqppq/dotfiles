57576709297b5a67e0c31997ca2b7588715b4679
pqppq <yutatsu0119@gmail.com>
2023-04-29 19:36:48 +0900

    chore: update

--- .config/nvim/lua/lazyvim/plugins/editor.lua
+++ .config/nvim/lua/lazyvim/plugins/editor.lua
@@ -268,6 +268,8 @@ return {
 							-- actions.which_key shows the mappings for your picker,
 							-- e.g. git_{create, delete, ...}_branch for the git_branches picker
 							["<C-h>"] = "which_key",
+							["<esc>"] = require('telescope.actions').close,
+							["jk"] = require('telescope.actions').close,
 						},
 					},
 					pickers = {

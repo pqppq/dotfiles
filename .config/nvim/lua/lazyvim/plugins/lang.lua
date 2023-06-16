61cc9df37b926a85447430d2d39ed8443404d57e
pqppq <yutatsu0119@gmail.com>
2023-06-16 12:38:54 +0900

    chore: update

--- .config/nvim/lua/lazyvim/plugins/lang.lua
+++ .config/nvim/lua/lazyvim/plugins/lang.lua
@@ -10,11 +10,11 @@ return {
 			vim.cmd('let g:go_code_completion_enabled = 0')
 			vim.cmd('let g:go_diagnostics_enabled = 0')
 
-			vim.cmd('autocmd FileType go nnoremap <Space>b  <Plug>(go-build)')
-			vim.cmd('autocmd FileType go nnoremap <Space>r  <Plug>(go-run)')
-			vim.cmd('autocmd FileType go nnoremap <Space>t  <Plug>(go-test)')
-			vim.cmd('autocmd FileType go nnoremap <Space>e  <Plug>(go-iferr)')
-			vim.cmd('autocmd FileType go nnoremap <Space>i  :GoImple')
+			vim.cmd('autocmd FileType go nnoremap gb  <Plug>(go-build)')
+			vim.cmd('autocmd FileType go nnoremap gr  <Plug>(go-run)')
+			vim.cmd('autocmd FileType go nnoremap gt  <Plug>(go-test)')
+			vim.cmd('autocmd FileType go nnoremap ge  <Plug>(go-iferr)')
+			vim.cmd('autocmd FileType go nnoremap gi  :GoImple')
 		end
 	},
 	{

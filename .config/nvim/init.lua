88931c77326d6e2bcd7083c09d477390dbaff699
pqppq <yutatsu0119@gmail.com>
2025-02-25 10:37:16 +0900

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -21,6 +21,9 @@ vim.api.nvim_set_keymap('v', 'K', ":move '<-2<CR>gv-gv", { noremap = true })
 
 vim.cmd('autocmd FileType go inoremap <C-s> :=')
 vim.cmd('autocmd FileType rust inoremap <C-s> ::')
+vim.cmd('autocmd FileType haskell inoremap <C-s> ::')
+vim.cmd('autocmd FileType haskell set expandtab')
+
 vim.cmd('filetype plugin indent on')
 vim.cmd("cnoreabbrev <expr> s getcmdtype() .. getcmdline() ==# ':s' ? [getchar(), ''][1] .. '%s///Ig<Left><Left><Left>' : 's'")
 

02009d79ec37f17b7467034ad40aa754ee7e3490
pqppq <yutatsu0119@gmail.com>
2025-03-15 01:43:11 +0900

    update

--- .config/nvim/lua/lazyvim/plugins/editor.lua
+++ .config/nvim/lua/lazyvim/plugins/editor.lua
@@ -365,7 +365,7 @@ return {
 				terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
 				persist_size = true,
 				persist_mode = true,  -- if set to true (default) the previous terminal mode will be remembered
-				direction = 'tab',    -- 'vertical' | 'horizontal' | 'tab' | 'float',
+				direction = 'float',    -- 'vertical' | 'horizontal' | 'tab' | 'float',
 				close_on_exit = true, -- close the terminal window when the process exits
 				shell = '/bin/zsh',   -- change the default shell
 				auto_scroll = true,   -- automatically scroll to the bottom on terminal output
